84d99aafcd7af9e84821269b68ad1b2154423b1c
KWADJOOWUSU-ANSAHQUARSHIE <97170718+KWADJOOWUSU-ANSAHQUARSHIE@users.noreply.github.com>
2024-01-12 19:27:03 +0000

    Delete server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-// express web server
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = process.env.PORT || 300;

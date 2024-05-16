66ae449b9a5a3e8b03ec8bbf164183eb9617c3bf
KWADJOOWUSU-ANSAHQUARSHIE <97170718+KWADJOOWUSU-ANSAHQUARSHIE@users.noreply.github.com>
2024-05-16 15:44:55 +0000

    Delete routes directory

--- routes/home.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-const homeController = require('../controllers/home');
-
-router.get('/', homeController.justHome);
-
-module.exports = router;
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-router.use('/', require('./swagger'));
-
-router.use('/', require('./home'));
-
-router.use('/contacts', require('./contacts'));
-
-module.exports = router;
